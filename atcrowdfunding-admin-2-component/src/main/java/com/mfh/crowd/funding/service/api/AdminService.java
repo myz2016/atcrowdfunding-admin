d400acccf36fc3fd4296cd4d391ca56eef56eb0e
myz2018_Service <myz2000@yeah.net>
2019-12-07 22:40:58 +0800

    1、在 Component 工程加入依赖 2、创建接口及实现类

---
+++ atcrowdfunding-admin-2-component/src/main/java/com/mfh/crowd/funding/service/api/AdminService.java
@@ -0,0 +1,8 @@
+package com.mfh.crowd.funding.service.api;
+
+/**
+ * @author mfh
+ * @date 2019/12/7 22:38
+ */
+public interface AdminService {
+}
---
+++ atcrowdfunding-admin-2-component/src/main/java/com/mfh/crowd/funding/service/impl/AdminServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mfh.crowd.funding.service.impl;
+
+import com.mfh.crowd.funding.service.api.AdminService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author mfh
+ * @date 2019/12/7 22:38
+ */
+@Service
+public class AdminServiceImpl implements AdminService {
+}
