86571f3e18ec689824579ab1a738b750fc218086
myz2018_Service <myz2000@yeah.net>
2020-02-20 17:26:53 +0800

    实体类创建

---
+++ atcrowdfunding-admin-2-component/src/main/java/com/mfh/crowd/funding/mapper/AuthMapper.java
@@ -0,0 +1,31 @@
+package com.mfh.crowd.funding.mapper;
+
+import com.mfh.crowd.funding.entity.Auth;
+import com.mfh.crowd.funding.entity.AuthExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AuthMapper {
+    int countByExample(AuthExample example);
+
+    int deleteByExample(AuthExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Auth record);
+
+    int insertSelective(Auth record);
+
+    List<Auth> selectByExample(AuthExample example);
+
+    Auth selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByExample(@Param("record") Auth record, @Param("example") AuthExample example);
+
+    int updateByPrimaryKeySelective(Auth record);
+
+    int updateByPrimaryKey(Auth record);
+}
