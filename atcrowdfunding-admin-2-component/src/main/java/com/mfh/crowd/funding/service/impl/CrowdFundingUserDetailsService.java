0b7b12ab1b9ca91dfe53af0388feb0726956665f
myz2018_Service <myz2000@yeah.net>
2020-12-26 13:37:42 +0800

    从数据库中获取相关的角色、权限

--- atcrowdfunding-admin-2-component/src/main/java/com/mfh/crowd/funding/service/impl/CrowdFundingUserDetailsService.java
+++ atcrowdfunding-admin-2-component/src/main/java/com/mfh/crowd/funding/service/impl/CrowdFundingUserDetailsService.java
@@ -2,17 +2,22 @@ package com.mfh.crowd.funding.service.impl;
 
 import com.mfh.crowd.funding.entity.Admin;
 import com.mfh.crowd.funding.entity.AdminExample;
+import com.mfh.crowd.funding.entity.Auth;
+import com.mfh.crowd.funding.entity.Role;
 import com.mfh.crowd.funding.mapper.AdminMapper;
+import com.mfh.crowd.funding.mapper.AuthMapper;
+import com.mfh.crowd.funding.mapper.RoleMapper;
 import com.mfh.crowd.funding.util.CrowdFundingUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +28,10 @@ import java.util.List;
 public class CrowdFundingUserDetailsService implements UserDetailsService {
     @Autowired
     private AdminMapper adminMapper;
+    @Autowired
+    private RoleMapper roleMapper;
+    @Autowired
+    private AuthMapper authMapper;
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
@@ -35,7 +44,33 @@ public class CrowdFundingUserDetailsService implements UserDetailsService {
         }
         Admin admin = admins.get(0);
         String password = admin.getUserPswd();
-        List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList("ROLE_学生", "ROLE_教授", "扫地");
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        // 查询当前 Admin 对应的角色
+        List<Role> roles = roleMapper.selectAssignedRoleList(admin.getId());
+        // 遍历
+        for (Role role : roles) {
+            // 注意：记得加前缀 ROLE_ !!!
+            String roleName = "ROLE_" + role.getName();
+            // 创建 SimpleGrantedAuthority 对象并加入集合
+            authorityList.add(new SimpleGrantedAuthority(roleName));
+        }
+
+        // 查询当前 Admin 对应的权限
+        List<Auth> auths = authMapper.selectAuthListByAdminId(admin.getId());
+        // 遍历
+        for (Auth auth : auths) {
+            // 注意：这里不加前缀！！！
+            String authName = auth.getName();
+            // 如果不是有效字符串，则抛弃
+            if (!CrowdFundingUtils.stringEffective(authName)) {
+                continue;
+            }
+            // 创建 SimpleGrantedAuthority 对象并加入集合
+            authorityList.add(new SimpleGrantedAuthority(authName));
+        }
+
+        // 返回 User 对象
         return new User(userName, password, authorityList);
     }
 }
